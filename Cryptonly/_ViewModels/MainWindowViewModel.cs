480fd170c5ebf20b565426c6b2bd703c46e97120
Vadim <113421299+XEducat@users.noreply.github.com>
2024-08-05 01:46:19 +0300

    Add MainWindowVM

---
+++ Cryptonly/_ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Cryptonly._Views;
+using Cryptonly.Views;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Cryptonly.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private readonly Frame _frame;
+        private ICommand _navigateToPopularCryptosCommand;
+        private ICommand _navigateToCryptoConverterCommand;
+        private ICommand _navigateToSettingsCommand;
+
+        public ICommand NavigateToPopularCryptosCommand => _navigateToPopularCryptosCommand ??= new RelayCommand(NavigateToPopularCryptos);
+        public ICommand NavigateToCryptoConverterCommand => _navigateToCryptoConverterCommand ??= new RelayCommand(NavigateToCryptoConverter);
+        public ICommand NavigateToSettingsCommand => _navigateToSettingsCommand ??= new RelayCommand(NavigateToSettings);
+
+        public MainWindowViewModel(Frame frame)
+        {
+            _frame = frame;
+            NavigateToPopularCryptos();
+        }
+
+        private void NavigateToPopularCryptos()
+        {
+            _frame?.Navigate(new PopularCryptosPage());
+        }
+
+        private void NavigateToCryptoConverter()
+        {
+            _frame?.Navigate(new CryptoConverterPage());
+        }
+
+        private void NavigateToSettings()
+        {
+            _frame?.Navigate(new SettingsPage());
+        }
+    }
+}
--- Cryptonly/_Views/MainWindow.xaml.cs
+++ Cryptonly/_Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Cryptonly._Views;
+using Cryptonly.ViewModels;
 using Cryptonly.Views;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,27 +14,16 @@ namespace Cryptonly
         public MainWindow()
         {
             InitializeComponent();
-            NavigateToPopularCryptos();
-        }
 
-        private void NavigateToPopularCryptos()
-        {
-            mainFrame.Navigate(new PopularCryptosPage());
-        }
-
-        private void CryptonlyButton_Click(object sender, RoutedEventArgs e)
-        {
-            NavigateToPopularCryptos();
-        }
-
-        private void CryptoConverterButton_Click(object sender, RoutedEventArgs e)
-        {
-            mainFrame.Navigate(new CryptoConverterPage());
-        }
-
-        private void SettingsButton_Click(object sender, RoutedEventArgs e)
-        {
-            mainFrame.Navigate(new SettingsPage());
+            var frame = App.Current.MainWindow.FindName("mainFrame") as Frame;
+            if (frame != null) 
+            {
+                DataContext = new MainWindowViewModel(frame);
+            }
+            else
+            {
+                throw new Exception("Frame on MainWindow not founded");
+            }
         }
     }
 }
